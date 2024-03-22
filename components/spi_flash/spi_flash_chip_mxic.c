0a3ad3731d4293eed673de928e4b2555284ebddf
Trent Piepho <tpiepho@gmail.com>
2024-03-21 17:45:32 -0700

    Support 32 bit addressing in MXIC driver
    Each SPI driver is different, despite being nearly identical with only a
    couple easily parameterized differences.
    
    So while the winbond driver already supports 32 bit address, the MXIC
    driver doesn't.  So we cut&paste the code into here.  But it has
    problems that need to be fixed.  And a bunch of junk in the read
    function with unneeded extra buffers and alignment is removed for a nice
    speedup.
    
    But none of the other drivers benefit, because they are separate.

--- components/spi_flash/spi_flash_chip_mxic.c
+++ components/spi_flash/spi_flash_chip_mxic.c
@@ -19,6 +19,9 @@
 #include "hal/spi_flash_hal.h"
 
 /* Driver for MXIC flash chip */
+static const char TAG[] = "chip_mcix";
+
+#define ADDR_32BIT(addr)            (addr >= (1<<24))
 
 esp_err_t spi_flash_chip_mxic_probe(esp_flash_t *chip, uint32_t flash_id)
 {
@@ -48,10 +51,108 @@ static const char chip_name[] = "mxic";
 spi_flash_caps_t spi_flash_chip_mxic_get_caps(esp_flash_t *chip)
 {
     spi_flash_caps_t caps_flags = 0;
-    // 32-bit-address flash is not supported
+    if ((chip->chip_id & 0xFF) >= 0x19) {
+        caps_flags |= SPI_FLASH_CHIP_CAP_32MB_SUPPORT;
+    }
     // flash-suspend is not supported
     // reading unique id is not supported.
     return caps_flags;
+}
+
+esp_err_t spi_flash_chip_mxic_detect_size(esp_flash_t *chip, uint32_t *size)
+{
+    *size = 1 << (chip->chip_id & 0xff);
+    return ESP_OK;
+}
+
+static esp_err_t spi_flash_command_mxic_program_page(esp_flash_t *chip, const void *buffer, uint32_t address, uint32_t length)
+{
+    esp_err_t err;
+
+    err = chip->chip_drv->wait_idle(chip, chip->chip_drv->timeout->idle_timeout);
+
+    if (err == ESP_OK) {
+        bool addr_4b = ADDR_32BIT(address);
+        spi_flash_trans_t t = {
+            .command = (addr_4b ? CMD_PROGRAM_PAGE_4B : CMD_PROGRAM_PAGE),
+            .address_bitlen = (addr_4b ? 32 : 24),
+            .address = address,
+            .mosi_len = length,
+            .mosi_data = buffer,
+            .flags = SPI_FLASH_TRANS_FLAG_PE_CMD,
+        };
+        err = chip->host->driver->common_command(chip->host, &t);
+        if (err == ESP_OK) {
+            chip->busy = 1;
+            err = chip->chip_drv->wait_idle(chip, chip->chip_drv->timeout->page_program_timeout);
+    }
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Program %02x @ %08lx = %d", ADDR_32BIT(address) ? CMD_PROGRAM_PAGE_4B : CMD_PROGRAM_PAGE_4B, address, err);
+    }
+    return err;
+}
+
+static esp_err_t spi_flash_command_mcix_erase(esp_flash_t *chip, uint32_t start_address, uint8_t cmd3, uint8_t cmd4, uint32_t timeout)
+{
+    esp_err_t err = chip->chip_drv->set_chip_write_protect(chip, false);
+    if (err == ESP_OK || err == ESP_ERR_NOT_SUPPORTED) {
+        err = chip->chip_drv->wait_idle(chip, chip->chip_drv->timeout->idle_timeout);
+        if (err == ESP_OK) {
+            //SET_FLASH_ERASE_STATUS(chip, SPI_FLASH_OS_IS_ERASING_STATUS_FLAG);
+
+            bool addr_4b = ADDR_32BIT(start_address);
+            spi_flash_trans_t t = {
+                .command = (addr_4b ? cmd4 : cmd3),
+                .address_bitlen = (addr_4b ? 32 : 24),
+                .address = start_address,
+                .flags = SPI_FLASH_TRANS_FLAG_PE_CMD,
+            };
+            err = chip->host->driver->common_command(chip->host, &t);
+            if (err == ESP_OK) {
+                chip->busy = 1;
+                err = chip->chip_drv->wait_idle(chip, timeout);
+            }
+            //SET_FLASH_ERASE_STATUS(chip, 0);
+        }
+    }
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Erase %02x @%08lx: Error %d", ADDR_32BIT(start_address) ? cmd4 : cmd3, start_address, err);
+    }
+    return err;
+}
+
+static esp_err_t spi_flash_command_mcix_erase_sector(esp_flash_t *chip, uint32_t start_address)
+{
+    return spi_flash_command_mcix_erase(chip, start_address, CMD_SECTOR_ERASE, CMD_SECTOR_ERASE_4B,
+                                        chip->chip_drv->timeout->sector_erase_timeout);
+}
+
+static esp_err_t spi_flash_command_mcix_erase_block(esp_flash_t *chip, uint32_t start_address)
+{
+    return spi_flash_command_mcix_erase(chip, start_address, CMD_LARGE_BLOCK_ERASE, CMD_LARGE_BLOCK_ERASE_4B,
+                                        chip->chip_drv->timeout->block_erase_timeout);
+}
+
+static esp_err_t spi_flash_chip_mcix_read(esp_flash_t *chip, void *buffer, uint32_t address, uint32_t length)
+{
+    esp_err_t err = ESP_OK;
+
+    // There is no init function so we have to do this on every single read!
+    err = chip->chip_drv->config_host_io_mode(chip, SPI_FLASH_CONFIG_IO_MODE_32B_ADDR);
+    if (err == ESP_ERR_NOT_SUPPORTED) {
+        ESP_LOGE(TAG, "configure host io mode failed - unsupported");
+    }
+
+    while (err == ESP_OK && length > 0) {
+	uint32_t read_len = MIN(length, 64); // Some kind of 64 byte limit!
+        err = chip->host->driver->read(chip->host, buffer, address, read_len);
+
+	address += read_len;
+	length -= read_len;
+	buffer += read_len;
+    }
+
+    return err;
 }
 
 // The mxic chip can use the functions for generic chips except from set read mode and probe,
@@ -61,10 +162,10 @@ const spi_flash_chip_t esp_flash_chip_mxic = {
     .timeout = &spi_flash_chip_generic_timeout,
     .probe = spi_flash_chip_mxic_probe,
     .reset = spi_flash_chip_generic_reset,
-    .detect_size = spi_flash_chip_generic_detect_size,
+    .detect_size = spi_flash_chip_mxic_detect_size,
     .erase_chip = spi_flash_chip_generic_erase_chip,
-    .erase_sector = spi_flash_chip_generic_erase_sector,
-    .erase_block = spi_flash_chip_generic_erase_block,
+    .erase_sector = spi_flash_command_mcix_erase_sector,
+    .erase_block = spi_flash_command_mcix_erase_block,
     .sector_size = 4 * 1024,
     .block_erase_size = 64 * 1024,
 
@@ -76,9 +177,9 @@ const spi_flash_chip_t esp_flash_chip_mxic = {
     .get_protected_regions = NULL,
     .set_protected_regions = NULL,
 
-    .read = spi_flash_chip_generic_read,
+    .read = spi_flash_chip_mcix_read,
     .write = spi_flash_chip_generic_write,
-    .program_page = spi_flash_chip_generic_page_program,
+    .program_page = spi_flash_command_mxic_program_page,
     .page_size = 256,
     .write_encrypted = spi_flash_chip_generic_write_encrypted,
 
