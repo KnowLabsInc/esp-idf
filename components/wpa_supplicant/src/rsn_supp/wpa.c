5198279fb0928b0ea63d4aef040acf9444fa53b1
Trent Piepho <tpiepho@gmail.com>
2024-04-09 17:34:13 -0700

    Allow compiling without wifi enterprise support enabled

--- components/wpa_supplicant/src/rsn_supp/wpa.c
+++ components/wpa_supplicant/src/rsn_supp/wpa.c
@@ -186,6 +186,8 @@ static bool is_wpa2_enterprise_connection(void)
 
     return false;
 }
+#else
+static inline bool is_wpa2_enterprise_connection(void) { return false; }
 #endif
 
 /**
@@ -655,6 +657,7 @@ void wpa_supplicant_process_1_of_4(struct wpa_sm *sm,
     u8 *kde, *kde_buf = NULL;
     size_t kde_len;
 
+#ifdef CONFIG_ESP_WIFI_ENTERPRISE_SUPPORT
     if (is_wpa2_enterprise_connection()) {
         wpa2_ent_eap_state_t state = eap_client_get_eap_state();
         if (state == WPA2_ENT_EAP_STATE_IN_PROGRESS) {
@@ -663,6 +666,7 @@ void wpa_supplicant_process_1_of_4(struct wpa_sm *sm,
             return;
         }
     }
+#endif
     wpa_sm_set_state(WPA_FIRST_HALF_4WAY_HANDSHAKE);
 
     wpa_printf(MSG_DEBUG, "WPA 1/4-Way Handshake");
